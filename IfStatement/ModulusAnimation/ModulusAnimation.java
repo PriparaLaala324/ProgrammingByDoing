f7382013a63b2024fd9d9a99139fef7021cc50b4
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2021-09-03 13:26:30 +0800

    ModulusAnimation

---
+++ IfStatement/ModulusAnimation/ModulusAnimation.java
@@ -0,0 +1,13 @@
+public class ModulusAnimation
+{
+  public static void main( String[] args ) throws Exception
+  {
+    for ( int i=0; i<80; i++ )
+    {
+      if (i%10 == 0 )
+        System.out.println( " Mr. Mitchell is cool. \r" );
+
+      Thread.sleep(200);
+    }
+  }
+}
