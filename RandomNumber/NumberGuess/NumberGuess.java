f1386c8585c46518bf0db3512ec04f8bcaf53c5f
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2021-09-14 13:47:03 +0800

    NumberGuess

---
+++ RandomNumber/NumberGuess/NumberGuess.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import java.util.Random;
+
+public class NumberGuess
+{
+  public static void main(String[] args)
+  {
+    Scanner keyboard = new Scanner(System.in);
+    Random r = new Random();
+
+    int choice;
+    int x = 1 + r.nextInt(10);
+
+    System.out.println("I'm thinking of a number from 1 to 10.");
+    System.out.print("Your guess: ");
+    choice = keyboard.nextInt();
+
+    System.out.print(1 + r.nextInt(10));
+    System.out.println();
+    if (choice == x)
+    {
+      System.out.println("That's right! My secret number was " + x + "!");
+    }
+    else if (choice != x)
+    {
+      System.out.println("Sorry, but I was thinking of " + x + ".");
+    }
+  }
+}
