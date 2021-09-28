69fed825e5be295243e2b77d052368a49e8557be
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2021-09-28 14:45:23 +0800

    blablabla

---
+++ ForLoops/CountingByHalves/CountingByHalves.java
@@ -0,0 +1,13 @@
+public class CountingByHalves
+{
+  public static void main(String[] args)
+  {
+    System.out.println("x");
+    System.out.println("------");
+    //double i;
+    for(double i=-10.0; i<=10.0; i+=0.5)
+    {
+      System.out.println(i);
+    }
+  }
+}
