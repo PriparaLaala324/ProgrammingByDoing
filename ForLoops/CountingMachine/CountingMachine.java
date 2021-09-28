e20228140feca2dcfee625aa0a8c5905d5bd201d
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2021-09-28 14:45:54 +0800

    no ; after for

---
+++ ForLoops/CountingMachine/CountingMachine.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CountingMachine
+{
+  public static void main(String[]args)
+  {
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("Count to: ");
+    int num = keyboard.nextInt();
+
+    for(int i=0; i<=num; i++)
+    {
+      System.out.print(i + " ");
+    }
+  }
+}
