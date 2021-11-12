8b7fd7d9346c2613676ca65301c57ef4ba93c2bc
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2021-11-12 14:36:43 +0800

    dfghj

---
+++ Array/BasicArrays1/BasicArrays1.java
@@ -0,0 +1,12 @@
+  public class BasicArrays1
+{
+  public static void main (String[]args)
+  {
+    int a[] = {-113, -113, -113, -113, -113, -113, -113, -113, -113, -113};
+
+    for (int i=0; i<a.length; i++)
+    {
+      System.out.println("Slot " +i+ " contains a " +a[i]);
+    }
+  }
+}
---
+++ Array/BasicArrays2/BasicArrays2.java
@@ -0,0 +1,12 @@
+public class BasicArrays2
+{
+  public static void main(String[]args)
+  {
+    int a[] = {45, 87, 39, 32, 93, 86, 12, 44, 75, 50};
+
+    for (int i=0; i<a.length; i++)
+    {
+      System.out.println("Slot " +i+ " contains a " +a[i]);
+    }
+  }
+}
