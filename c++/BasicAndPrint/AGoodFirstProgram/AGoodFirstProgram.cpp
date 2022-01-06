fa3df1d38b7ca5866a858bff844d940afa7e815f
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2022-01-06 23:19:15 +0800

    c++2

---
+++ c++/BasicAndPrint/AGoodFirstProgram/AGoodFirstProgram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!";
+    cout << "Hello Again";
+    cout << "I like typing this.";
+    cout << "This is fun.";
+    cout << "Yay! Printing.";
+    cout << "I'd much rather you 'not'.";
+    cout << "I \"said\" do not touch this.";
+}
