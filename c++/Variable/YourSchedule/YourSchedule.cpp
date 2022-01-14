1e8602bfbde8ae83c7e3746527f0921dd9897315
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2022-01-14 14:46:11 +0800

    xcvbnm,

---
+++ c++/Variable/YourSchedule/YourSchedule.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"+-----------------------------------------------------------+\n";
+    cout<<"| 1 |                        English III  |        Ms.Lapan |\n";
+    cout<<"| 2 |                         Precalculus |     Mrs. Gideon |\n";
+    cout<<"| 3 |                        Music Theory |        Mr.Davis |\n";
+    cout<<"| 4 |                       Biotechnology |      Mr. Palmer |\n";
+    cout<<"| 5 |          Principles of Technology I |      Ms. Garcia |\n";
+    cout<<"| 6 |                           Latin II  |    Mrs. Barnett |\n";
+    cout<<"| 7 |                       AP US History | Ms. Jojannessen |\n";
+    cout<<"| 8 | Business Computer Infomation System |       Mr. James |\n";
+    cout<<"+-----------------------------------------------------------+\n";
+}
