fa771e90cfae4b975f3328ec057726f4b9164531
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2022-01-16 19:09:16 +0800

    dfghjk

---
+++ c++/KeyboardInput/BMICalculator/BMICalculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float bmi, w, h;
+    cout<<"Please enter your weight in kg: \n";
+        cin>>w;
+    cout<<"Please enter your height in cm: \n";
+        cin>>h;
+    bmi=(w)/(h*h);
+    cout<<"Your BMI is: \n"<<bmi<<endl;
+
+    return 0;
+}
