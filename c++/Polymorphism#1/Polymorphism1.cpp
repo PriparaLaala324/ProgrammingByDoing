a9f0b90706e4a621acae5e3c0e2e8e0e1dbca4c4
PriparaLaala324 <89628796+PriparaLaala324@users.noreply.github.com>
2022-03-08 13:52:17 +0800

    sdfghkl;

---
+++ c++/Polymorphism#1/Polymorphism1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class findArea{
+    public:
+        findArea(){}
+        findArea(int x){
+            cout<<x<<endl;
+        }
+        findArea(int w, int h){
+            cout<<h*w<<endl;
+        }
+        findArea(int b1, int b2, int h){
+            cout<<b1*b2*h<<endl;
+        }
+};
+int main(){
+    findArea(10);
+    findArea(10, 20);
+    findArea(10, 20, 30);
+    return 0;
+}
